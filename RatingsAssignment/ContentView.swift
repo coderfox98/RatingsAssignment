9638a4f71b4fa4c384798f094ed8cc4708c6554a
Rishabh Raj <rishabh.raj@studiomosaicapps.com>
2020-05-20 10:31:28 +0530

    Initial Commit

---
+++ RatingsAssignment/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RatingsAssignment
+//
+//  Created by Rishabh Raj on 20/05/20.
+//  Copyright © 2020 Rishabh Raj. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
